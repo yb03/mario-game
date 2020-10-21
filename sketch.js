4b7d2004e6975432b8ff162f6ad8b5acf6bc664f
yb03 <69167189+yb03@users.noreply.github.com>
2020-10-21 16:53:22 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,15 +5,25 @@ var marioimg
 var jump
 var obstacle
 var brickimg
+var obstacleGroup
+var bricksGroup
+var gameState="play"
+var score=0
+var gameover
+var restart
 
 function preload () 
 {
 bg=loadImage("bg.png")
 groundimg=loadImage("ground2.png")
 marioimg=loadAnimation("mario00.png", "mario01.png", "mario02.png", "mario03.png")
+mariocollided=loadAnimation("collided.png")
+die=loadSound("die.mp3")
 jump=loadSound("jump.mp3");
 obstacleimg=loadAnimation("obstacle1.png", "obstacle2.png", "obstacle3.png", "obstacle4.png")
-brickimg=loadImage("brick.png")
+brickimg=loadImage("coin.png")
+gameoverimg=loadImage("gameOver.png")
+restartimg=loadImage("restart.png")
 }
 function setup()
 {
@@ -21,33 +31,78 @@ createCanvas(1200,600)
 ground=createSprite(600,550,1200,100)
 ground.scale=2
 ground.addImage("groundimg",groundimg)
-ground.velocityX=-5
+ground.velocityX=-4
 ground.x=ground.width/2
 mario=createSprite(300, 440, 50,40)
 mario.addAnimation("marioimg",marioimg)
-mario.scale=2.2
+mario.scale=2
+mario.addAnimation("collided", mariocollided)
+mario.setCollider("circle", 0,0, 15)
+mario.debug=true
+bricksGroup=new Group()
+obstacleGroup= new Group()
+gameover=createSprite(600,300,50,50)
+restart=createSprite(600,200,30,30)
+restart.addImage("restartimg", restartimg)
+gameover.addImage("gameoverimg", gameoverimg)
+gameover.visible=false
+restart.visible=false
 }
 function draw()
 {
-    
-    background(bg)
+    background(bg);
+     drawSprites()
+     textSize(20)
+     fill("BLACK")
+     text("Score="+score, 800,100) 
+if (gameState==="play"){
 
-
-    if (keyDown("space")&& mario.y>0)
-    {
-        mario.velocityY=-20
-        jump.play()
-        console.log("mario.velocityY")
+    if(keyDown("SPACE") && mario.y >= 400) { 
+        mario.velocityY = -13 ; 
+        jump.play(); 
     }
     mario.velocityY=mario.velocityY+1;
-    mario.collide(ground)
     if (ground.x<0)
     {
         ground.x=ground.width/2
     }
+    for (var index = 0; index < bricksGroup.length; index++) {
+       if(bricksGroup.get(index).isTouching(mario)){
+        bricksGroup.get(index).remove();
+        score=score+1;
+       }
+
+    }
     spawnObstacles();
     spawnBricks();
-    drawSprites()
+    if(obstacleGroup.isTouching(mario)){
+        die.play()
+    gameState="end"
+    }
+}
+ else if(gameState==="end"){
+    ground.velocityX=0;
+    mario.velocityX=0;
+    mario.velocityY=0;
+ //    jump.play() 
+    mario.changeAnimation("collided",mariocollided);
+    obstacleGroup.setVelocityXEach(0);
+    bricksGroup.setVelocityXEach(0);
+    gameover.visible=true
+    restart.visible=true
+}
+if(mousePressedOver(restart)){
+    gameState="play"
+    gameover.visible=false
+    restart.visible=false
+    obstacleGroup.destroyEach()
+    bricksGroup.destroyEach()
+    mario.changeAnimation("marioimg", marioimg)
+    ground.velocityX=-4
+    score=0
+
+}
+    mario.collide(ground)
 }
 
 function spawnObstacles()
@@ -57,15 +112,24 @@ function spawnObstacles()
     var obstacle=createSprite(1100, 440, 50,40)
     obstacle.velocityX=-4
     obstacle.addAnimation("obstacleimg", obstacleimg)
-    obstacle.scale=1.5
+    obstacle.scale=1
+    obstacle.setCollider("circle", 0,0, 20)
+obstacle.debug=true
+    obstacleGroup.add(obstacle)
+
+
+
 }
 }
 function spawnBricks()
 {
+
     if (frameCount%80===0){
         var bricks=createSprite(600, 300, 50, 50)
-        bricks.velocityX=-3
+        bricks.velocityX=-4
         bricks.addImage("brickimg", brickimg)
-        bricks.scale=1
+        bricks.scale=0.15
+        bricks.y=random(400,300)
+        bricksGroup.add(bricks)
     }
 }
